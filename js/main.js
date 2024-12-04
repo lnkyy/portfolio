c219869fdb618a1eb9897175fc7227897276a3b8
lnkyy <mariusmoreau04@gmail.com>
2024-12-04 10:03:53 +0100

    🎨 maj strucute et import pour test three

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-import * as THREE from 'three';
-import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
-import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader';
+import * as THREE from '../node_modules/three';
+import {OrbitControls} from '../node_modules/three/examples/jsm/controls/OrbitControls';
+import {GLTFLoader} from '../node_modules/three/examples/jsm/loaders/GLTFLoader';
 
 function init(){ 
 
