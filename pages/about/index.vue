737dfa4f1d2846d66347e28ec2fb990854d766e6
lnkyy <mariusmoreau04@gmail.com>
2025-01-07 18:32:40 +0100

    🚧 feat : ajout base détail projet + base tiles toutes les technos que je connais + maj JSON projets (mettre à jour pour tous les projets)

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,25 +1,20 @@
 <template>
+
     <Head>
         <Title>à propos</Title>
     </Head>
     <Header></Header>
     <navBar></navBar>
     <main class="main" id="about">
-        <div class="about-container" >
+        <div class="about-container">
             <div class="quatre-vingt">
                 <div class="about-block  block-info">
                     <div class="un-tier intro">
-                        <NuxtImg 
-                            src="/images/Marius.jpg"
-                            alt="image de ma tête"
-                            :placeholder="[50, 25, 75, 5]"
-                            fit="inside"
-                            id="container-pp-about"
-                            loading="lazy"
-                            />
+                        <NuxtImg src="/images/Marius.jpg" alt="image de ma tête" :placeholder="[50, 25, 75, 5]"
+                            fit="inside" id="container-pp-about" loading="lazy" />
                     </div>
-                    <p class="separateur-vertical" id="about-sep" ></p>
-                    <hr class="separateur-plat" id="about-sep-plat"/>
+                    <p class="separateur-vertical" id="about-sep"></p>
+                    <hr class="separateur-plat" id="about-sep-plat" />
                     <div class="deux-tier intro" id="container-texte">
                         <p class="text-about">{{ texteIntro }}</p>
                     </div>
@@ -29,17 +24,14 @@
                 </div>
             </div>
             <div class="vingt">
-                <NuxtImg 
-                    src="/images/moi_transparent.png"
-                    alt="moi en dresseur de pokémon"
-                    :placeholder="[50, 25, 75, 5]"
-                    fit="inside"
-                    id="moi-dresseur"
-                    loading="lazy"
-                    />
+                <NuxtImg src="/images/moi_transparent.png" alt="moi en dresseur de pokémon"
+                    :placeholder="[50, 25, 75, 5]" fit="inside" id="moi-dresseur" loading="lazy" />
             </div>
         </div>
-        <div  class=" block-info about-block column">
+        <div class="block-info about-block">
+            <p>Ajouter tout les langages que j'ai déjà utilisé à ajouter dans un nouveau Json</p>
+        </div>
+        <div class=" block-info about-block column">
             <div class="cent">
                 <div>
                     <label for="radioEtudes" class="labelReseaux">
@@ -51,20 +43,20 @@
                         <span>Réseaux</span>
                     </label>
                 </div>
-                <hr class="separateur-plat"/>
+                <hr class="separateur-plat" />
             </div>
             <div v-if="aboutRadio === 'Etudes'" class="container-tiles-reseaux">
                 <h2 class="block-gauche">Mes Études</h2>
                 <div class="list-etudes">
-                    <div >
+                    <div>
                         <p>
                             <span>- 2021 - 2022 : </span>
                             <span>Bac général Spécialité NSI & Mathématique mention Assez bien</span>
-                        </p>   
+                        </p>
                         <p>
                             <span>- 2021 - 2022 : </span>
                             <span>Diplome du Cambridge Niveau B2</span>
-                        </p>   
+                        </p>
                         <p>
                             <span>- 2022 - 2025 : </span>
                             <span>But informatique à l'iut de Lannion</span>
@@ -72,7 +64,7 @@
                         <p>
                             <span>- 2023 - 2025 : </span>
                             <span>Alternant à CGi</span>
-                        </p>     
+                        </p>
                     </div>
                 </div>
             </div>
@@ -81,18 +73,14 @@
                 <div class="container-tiles">
                     <div v-for="reseau in reseaux" class="tile-reseau">
                         <NuxtLink :to="reseau.lien" class="tile-reseau-lien">
-                            <NuxtImg
-                                :src="reseau.logo"
-                                :alt="`Logo de ${reseau.nom}`"
-                                class="img-reseau"
-                                loading="lazy"
-                            />
+                            <NuxtImg :src="reseau.logo" :alt="`Logo de ${reseau.nom}`" class="img-reseau"
+                                loading="lazy" />
                             <p>{{ reseau.nom }}</p>
                         </NuxtLink>
                     </div>
                 </div>
             </div>
-        </div> 
+        </div>
     </main>
     <Footer></Footer>
 </template>
--- pages/projets/[idProjet].vue
+++ pages/projets/[idProjet].vue
@@ -1,25 +1,51 @@
 <template>
+
   <Head>
-        <Title>Projets</Title>
-    </Head>
-    <Header></Header>
-    <navBar></navBar>
-    <main id="projet-id" class="main">
-
-        
-
+    <Title>Projets</Title>
+  </Head>
+  <Header></Header>
+  <navBar></navBar>
+  <main id="projet-id" class="main">
+    <div class="container-detail">
+      <div class="container-detail-projet">
+        <h2 class="titre-detail-projet">{{ projet.titre }}</h2>
+        <div class="container-tag-img-projet">
+          <div class="container-role-tag">
+            <div v-for="role in projet.role" class="role-tag">
+              <p>{{ role }}</p>
+            </div>
+          </div>
+          <div class="container-banner-projet">
+            <NuxtImg :src="projet.banner.img" :alt="projet.banner.alt" :placeholder="[50, 25, 75, 5]" fit="inside"
+              class="banner-projet" loading="lazy" />
+          </div>
+          <div class="container-projet-tile">
+            <p>projets lié à ajouter sous forme de boucle une fois la page faites pour les JSON</p>
+          </div>
+        </div>
+      </div>
+      <div class="container-detail-projet">
         <div class="container-tile-tag">
-        <div v-for="role in projet.role" class="tile-tag">
-            <p>{{ role }}</p>
+          <div v-for="tag in projet.tags" class="tile-tag">
+            <p>{{ tag }}</p>
+          </div>
         </div>
+        <div>
+          <h3>Objectif</h3>
+          <p class="texteBrut"> {{ projet.objectif }}</p>
         </div>
-        <div class="container-tile-tag">
-            <div v-for="techno in projet.technos" class="tile-tag">
-                <p>{{ techno.titre }}</p>
+        <div>
+          <h3>Description</h3>
+          <p v-for="p in projet.description">{{ p.texte }}</p>
         </div>
+        <div>
+          <p>slider des technos</p>
+          <p v-for="p in projet.technos" > {{ p.titre }} : {{ p.pourcentage }}</p>
+        </div>
+      </div>
     </div>
-    </main>
-    <Footer></Footer>
+  </main>
+  <Footer></Footer>
 </template>
 <script setup>
   import '~/assets/projet.css'
@@ -42,5 +68,33 @@
     }
   });
 
+  /*
+  
+  <div class="container-title">
+        <h2>{{ projet.titre }}</h2>
+      </div>
+      <div class="projet-detail">
+        <div>
+          <span class="texteBrut"><strong>Objectif : </strong>{{ projet.objectif }}</span>
+        </div>
+        <div v-for=" descr in projet.description">
+          <p class="texteBrut">{{ descr.texte }}</p>
+        </div>
+        <div class="container-tile-role">
+          <h3 class="role" v-if="projet.role.length > 1">Mes rôles : </h3>
+          <h3 v-if="projet.role.length == 1">Mon rôle</h3>
+
+          <div v-for="role in projet.role" class="role-tag">
+            <p>{{ role }}</p>
+          </div>
+        </div>
+        <div class="container-tile-tag">
+          <div v-for="techno in projet.technos" class="tile-tag">
+            <p>{{ techno.titre }}</p>
+          </div>
+        </div>
+      </div>
+  
+  */
 
 </script>
