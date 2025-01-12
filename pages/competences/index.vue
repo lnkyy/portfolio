3f0763bd5fd8e7e6d824733d29798deb90cdfbd8
lnkyy <mariusmoreau04@gmail.com>
2025-01-12 13:21:28 +0100

    ✨ Ajout base competences IUT

--- pages/competences/index.vue
+++ pages/competences/index.vue
@@ -1,20 +1,29 @@
 <template>
+
     <Head>
         <Title>Compétences</Title>
     </Head>
     <Header></Header>
     <navBar></navBar>
-    <div >
-        <div>
-            <h1>Compétences</h1>
+    <main id="comp-main" class="main">
+        <div class="container-list-competences">
+            <div v-for="competence in listComp" class="container-competence">
+                <p>{{ competence }}</p>
+            </div>
         </div>
-    </div>
+    </main>
     <Footer></Footer>
 </template>
 <script setup>
+    import '~/assets/competences.css'
+    import comp from '~/static/data/competences_iut.json'
+
+
     import Header from '../components/header.vue'
     import navBar from '../components/navBar.vue';
     import Footer from '../components/footer.vue';
+
+    let listComp = ref(comp);
 
 </script>
 
